313c1a44a7332e3731277fa57f734faf9b5ef193
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-03-08 22:29:27 +0530

    refactor: Update list data structure and rendering
    - Change the  array to store objects with uid=1000(nuwan) gid=1000(nuwan) groups=1000(nuwan),4(adm),24(cdrom),27(sudo),30(dip),46(plugdev),122(lpadmin),135(lxd),136(sambashare) and  properties
    - Update the  function to create list elements with  attribute
    - Set the  attribute value to the uid=1000(nuwan) gid=1000(nuwan) groups=1000(nuwan),4(adm),24(cdrom),27(sudo),30(dip),46(plugdev),122(lpadmin),135(lxd),136(sambashare) property of the list object
    - No changes made to the  function
    
    This refactor improves the data structure by introducing a unique identifier (uid=1000(nuwan) gid=1000(nuwan) groups=1000(nuwan),4(adm),24(cdrom),27(sudo),30(dip),46(plugdev),122(lpadmin),135(lxd),136(sambashare)) for each list, allowing for better data management and future enhancements, such as list manipulation or persistence.

--- src/todolist.js
+++ src/todolist.js
@@ -1,13 +1,21 @@
 const listsContainer = document.querySelector('[data-lists]');
 
-let lists = ['Study', 'Washing'];
+let lists = [{
+    id:1,
+    name:'Study'
+    },
+    {
+    id:2,
+    name:'Make tutorials'
+    }];
 
 function render(){
     clearElement(listsContainer);
     lists.forEach(list =>{
         const listElement = document.createElement('li');
+        listElement.dataset.listId = list.id;
         listElement.classList.add("list-name");
-        listElement.innerHTML = list;
+        listElement.innerHTML = list.name;
         listsContainer.appendChild(listElement);
     })
 }
