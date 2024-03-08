17206e5555e58bcfa6d761bd6e5573eb8448d555
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-03-08 22:52:52 +0530

    feat: Add functionality to create new lists
    - Select the new list form and input elements from the DOM
    - Add an event listener to the new list form for the 'submit' event
    - In the event listener function:
      - Prevent the default form submission behavior
      - Get the value of the new list input field
      - Check if the input value is empty or null, and return early if so
      - Call a new function  with the input value as an argument
    - The  function is not implemented yet

--- src/todolist.js
+++ src/todolist.js
@@ -1,4 +1,6 @@
 const listsContainer = document.querySelector('[data-lists]');
+const newListForm = document.querySelector('[data-new-list-form]');
+const newListInput = document.querySelector('[data-new-list-input]');
 
 let lists = [{
     id:1,
@@ -7,7 +9,15 @@ let lists = [{
     {
     id:2,
     name:'Make tutorials'
-    }];
+    }
+];
+
+newListForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const listName = newListInput.value;
+    if(listName == null || listName === '') return;
+    const list = createList(listName);
+})
 
 function render(){
     clearElement(listsContainer);
