50a5bb305babe65cf1f848dae347bc85915d299a
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-03-17 22:39:49 +0530

    Add event listener for new task submission

--- src/todolist.js
+++ src/todolist.js
@@ -7,6 +7,10 @@ const listDisplayContainer = document.querySelector('[data-list-display-containe
 const listTitleElement = document.querySelector('[data-list-title]');
 const listCountElement = document.querySelector('[data-list-count]');
 const tasksContainer = document.querySelector('[data-tasks]');
+const taskTemplate = document.getElementById('task-template');
+const newTaskForm = document.querySelector('[data-new-task-form]');
+const newTaskInput = document.querySelector('[data-new-task-input]');
+
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists';
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
@@ -36,6 +40,17 @@ newListForm.addEventListener('submit', e => {
     lists.push(list);
     saveAndRender();
 })
+
+newTaskForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const taskName = newTaskInput.value
+    if (taskName == null || taskName === '') return
+    const task = createTask(taskName)
+    newTaskInput.value = null
+    const selectedList = lists.find(list => list.id === selectedListId)
+    selectedList.tasks.push(task)
+    saveAndRender()
+  })
 
 function createList(name){
     return {id:Date.now().toString(), name:name, tasks:[]};
